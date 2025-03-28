6d06ca18f76228027b5d0965834f4bcab5b3ef76
Shawn <shawn.dev.vn@gmail.com>
2025-03-28 14:59:19 +0100

    Linting: Standardize string quotes and format spacing
    This commit refactors the `pg_model.py` file by standardizing the use of double quotes for string literals and adjusting spacing around type annotations and default values for better readability. These changes improve code consistency and adhere to PEP 8 style guidelines.

--- src/pgjinja/pg_model.py
+++ src/pgjinja/pg_model.py
@@ -1,21 +1,21 @@
-from typing import LiteralString, Any
+from typing import Any, LiteralString
 
 from pydantic import BaseModel
 
 
 class PgModel(BaseModel):
-    __table_name__: str = ''
+    __table_name__: str = ""
 
-    def save(self, on_conflict: LiteralString['do nothing', 'do update'] = 'do update'):
+    def save(self, on_conflict: LiteralString["do nothing", "do update"] = "do update"):
         pass
 
     def delete(self):
         pass
 
     @classmethod
-    def select(cls, limit: int=0, offset: int=0, where:dict[str, Any]=None):
+    def select(cls, limit: int = 0, offset: int = 0, where: dict[str, Any] = None):
         pass
 
     @classmethod
-    def get_table_name(cls)->str:
+    def get_table_name(cls) -> str:
         return cls.__table_name__ or cls.__name__.lower()
